f39754ae6032293523f17ddd5d2ab45f5f32ac94
twocucao <twocucao@gmail.com>
2021-06-17 17:45:15 +0800

    use context local like api

--- tifa/contrib/db.py
+++ tifa/contrib/db.py
@@ -1,23 +1,19 @@
 from __future__ import annotations
 
-import typing as t
-from contextvars import ContextVar
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.future import select
-from sqlalchemy.orm import sessionmaker, as_declarative
+from sqlalchemy.orm import sessionmaker
 
+from tifa.contrib.globals import glb
 from tifa.settings import get_settings
-
-_session: ContextVar[t.Optional[AsyncSession]] = ContextVar("_session", default=None)
 
 
 class SQLAlchemy:
-    Model: t.Type[BaseModel]
+    # Model: t.Type[BaseModel]
     Session: sessionmaker
 
-    def __init__(self):
-        self.Model = BaseModel
+    def __init__(self, Model):
+        self.g = glb
+        self.Model = Model
         self.engine = self.create_engine()
         self.Session = sessionmaker(
             self.engine, class_=AsyncSession, expire_on_commit=False
@@ -37,35 +33,6 @@ class SQLAlchemy:
 
     @property
     def session(self) -> AsyncSession:
-        """Return an instance of Session local to the current async context."""
-        session = _session.get()
-        if session is None:
-            session = self.Session()
-            _session.set(session)
-        return session
-
-
-def _fetch_local_session():
-    from tifa.globals import db
-    return db.session
-
-
-@as_declarative()
-class BaseModel:
-    __mapper_args__ = {"eager_defaults": True}
-
-    # @declared_attr
-    # def __tablename__(cls):
-    #     return cls.__name__.lower()
-
-    @classmethod
-    async def add(cls, **kwargs) -> BaseModel:
-        obj = cls(**kwargs)
-        session = _fetch_local_session()
-        session.add(obj)
-        return obj
-
-    @classmethod
-    async def all(cls, **kwargs) -> list[BaseModel]:
-        session = _fetch_local_session()
-        return (await session.execute(select(cls).where(**kwargs))).scalars().all()
+        if not self.g.session:
+            self.g.session = self.Session()
+        return self.g.session
