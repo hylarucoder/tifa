9634a311086fd74e9d7f02a1ef53999f868d017e
twocucao <twocucao@gmail.com>
2021-07-26 18:21:59 +0800

    trace

--- tifa/apps/health/__init__.py
+++ tifa/apps/health/__init__.py
@@ -31,13 +31,9 @@ def test_sentry():
 
 @bp.get("/test_1_plus_1/{result}")
 def test_1_plus_1(result: int):
-    return {
-        "result": result == 2
-    }
+    return {"result": result == 2}
 
 
 @bp.get("/test/1_plus_1_{result}")
 def test_1_plus_1_v2(result: int):
-    return {
-        "result": result == 2
-    }
+    return {"result": result == 2}
